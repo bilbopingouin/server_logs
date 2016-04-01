9c05e5104a9461442979f6c20dbc3e6b0d09e448
TLB <t.lebleis@proton-motor.de>
2016-04-01 22:42:34 +0200

    Analysis of the different parts of /var/log/auth.log

--- auth/analyse_auth.sh
+++ auth/analyse_auth.sh
@@ -3,22 +3,6 @@
 
 extract_all.sh   | sed 's/^Jan/01/' | sed 's/^Feb/02/' | sed 's/^Mar/03/' | sed 's/^Apr/04/' | sed 's/^May/05/' | sed 's/^Jun/06/' | sed 's/^Jul/07/' | sed 's/^Aug/08/' | sed 's/^Sep/09/' | sed 's/^Oct/10/' | sed 's/^Nov/11/' | sed 's/^Dec/12/' > tmp/data.csv
 
-
-#
-#echo "#========================================" 
-#echo "# $(date)" 
-#
-#echo "" 
-#echo "# Total connection(s) attempts?" 
-#cat $IN | grep -v '#' | awk '{print $7}' | sort | uniq -c | sort -nr 
-#
-#echo ""
-#echo "# Type of connections" 
-#cat $IN | grep -v '#' | awk '{print $6}' | sort | uniq -c | sort -nr 
-#
-#echo ""
-#echo "# Successful"
-#cat $IN | grep -v '#' | awk '{print $6, $7, $1, $2, $3, $4, $5}' | grep '^\ *7'
-
-
 Rscript analyse_data.R
+
+cat tmp/usernames.txt | sed -n 's/^"\(\w\+\)",\([0-9]\+\)/\1\ (\2),\ /p' > out/usenames.txt
--- auth/analyse_data.R
+++ auth/analyse_data.R
@@ -8,12 +8,9 @@ months_length <- c(31,29,31,30,31,30,31,31,30,31,30,31);
 first_element <- data[1,];
 
 time <- ((12+data$mon-first_element$mon)%%12)*months_length[first_element$mon]  + (data$day-first_element$day) + (data$h) / (24.0)  + (data$m) / (24.0*60.0);
-#time <- (data$mon-first_element$mon)*months_length[first_element$mon]  + (data$day-first_element$day) + (data$h-first_element$h) / (24.0)  + (data$m-first_element$m) / (24.0*60.0);
 
-#str(time)
 data$timestamp <- time;
 
-#str(data)
 max_time = floor(max(data$timestamp))+1;
 time_bins <- c(0:(max_time*24))/24;
 
@@ -21,64 +18,96 @@ time_bins <- c(0:(max_time*24))/24;
 # Function to plot the different parts
 plot_df = function(df,name,color="blue") 
 {
-  # Print activity
-  act_file <- paste("out/",name,"_hist.png",sep="")
-  png(filename=act_file, width=860);
-  hist(df$timestamp, breaks=time_bins, xlim=c(0,max_time), xlab="Time [days]", col=color, main="");
-  dev.off(); 
+  if (length(df$ip)>1)
+  {
 
-  # Print IPs histogram
-  ip_file <- paste("out/",name,"_ips.png",sep="")
-  png(filename=ip_file, width=860);
-  barplot(sort(table(as.character(df$ip)), decreasing=TRUE), col=color, xaxt='n');
-  dev.off();
+    # Print activity
+    act_file <- paste("out/",name,"_hist.png",sep="")
+    png(filename=act_file, width=860);
+    hist(df$timestamp, breaks=time_bins, xlim=c(0,max_time), xlab="Time [days]", col=color, main="");
+    dev.off(); 
 
-  # Print top 5
-  par(cex.axis=0.8)
-  topip_file <- paste("out/",name,"_ips_top.png",sep="")
-  png(filename=topip_file, width=640);
-  barplot(sort(table(as.character(df$ip)), decreasing=TRUE)[1:5], col=color);
-  dev.off();
-  par(cex.axis=1)
+    # Print IPs histogram
+    ip_file <- paste("out/",name,"_ips.png",sep="")
+    png(filename=ip_file, width=860);
+    barplot(sort(table(as.character(df$ip)), decreasing=TRUE), col=color, xaxt='n');
+    dev.off();
+
+    # Print top 5
+    par(cex.axis=0.8)
+    topip_file <- paste("out/",name,"_ips_top.png",sep="")
+    png(filename=topip_file, width=640);
+    barplot(sort(table(as.character(df$ip)), decreasing=TRUE)[1:5], col=color);
+    dev.off();
+    par(cex.axis=1)
+  }
+}
+
+plot_usernames = function(df,name,color="blue")
+{
+  if (length(df$var) > 0)
+  {
+    # Print usernames histogram
+    ip_file <- paste("out/",name,"_usr.png",sep="")
+    png(filename=ip_file, width=860);
+    barplot(sort(table(as.character(df$var)), decreasing=TRUE), col=color, xaxt='n');
+    dev.off();
+
+    # Print top 5
+    par(cex.axis=0.8)
+    topip_file <- paste("out/",name,"_usr_top.png",sep="")
+    png(filename=topip_file, width=640);
+    barplot(sort(table(as.character(df$var)), decreasing=TRUE)[1:5], col=color);
+    dev.off();
+    par(cex.axis=1)
+  }
 }
 
 #============================================
 # Analyse succesful logings
 data_success <- data[data$type == 7,]
-#png(filename="out/accepted_hist.png", width=860);
-#hist(data_success$timestamp, breaks=time_bins, xlim=c(0,max_time), xlab="Time [days]", col="red", main="");
-#dev.off(); 
-#
-#png(filename="out/accepted_ips.png",width=640);
-#barplot(table(as.character(data_success$ip)), col="red");
-#dev.off();
 plot_df(data_success,"accepted","red")
 
 
 #============================================
 # General activity not accepted
 data_denied <- data[data$type != 7,]
-
 plot_df(data_denied,"denied")
 
-## Print activity
-#png(filename="out/denied_hist.png", width=860);
-#hist(data_denied$timestamp, breaks=time_bins, xlim=c(0,max_time), xlab="Time [days]", col="blue", main="");
-#dev.off(); 
-#
-## Print IPs histogram
-#png(filename="out/all_ips_denied.png", width=860);
-#barplot(sort(table(as.character(data_denied$ip)), decreasing=TRUE), col="blue", xaxt='n');
-#dev.off();
-#
-## Print top 5
-#par(cex.axis=0.8)
-#png(filename="out/top_ips_denied.png", width=640);
-#barplot(sort(table(as.character(data_denied$ip)), decreasing=TRUE)[1:5], col="blue");
-#dev.off();
-#par(cex.axis=1)
+#============================================
+# Analyse of invalid logings
+data_invalid <- data[data$type == 1,]
+plot_df(data_invalid,"invalid","#ff8c00")
+plot_usernames(data_invalid,"invalid","#ff8c00")
 
-# histograms time but selecting grounds 1 to 6
-# list successful log in
-# list the 10 most persistent IPs
-# list all the attempted usernames
+#============================================
+# Analyse of failed logings
+data_failed <- data[data$type == 2,]
+plot_df(data_failed,"failed","#ff8c00")
+plot_usernames(data_failed,"failed","#cc00f8")
+
+#============================================
+# Analyse of no_id_string logings
+data_no_id_string <- data[data$type == 3,]
+plot_df(data_no_id_string,"no_id_string","#00b135")
+
+#============================================
+# Analyse of root_refused logings
+data_root_refused <- data[data$type == 4,]
+plot_df(data_root_refused,"root_refused","#b16c00")
+
+#============================================
+# Analyse of break_in logings
+data_break_in <- data[data$type == 5,]
+plot_df(data_break_in,"break_in","#02deff")
+
+#============================================
+# Analyse of disconnect logings
+data_disconnect <- data[data$type == 6,]
+plot_df(data_disconnect,"disconnect","#f0ff02")
+
+#============================================
+# Usernames
+usernames <-sort(table(as.character(data[data$var != 0,]$var)),decreasing=TRUE)
+write.csv(usernames,file="tmp/usernames.txt")
+
